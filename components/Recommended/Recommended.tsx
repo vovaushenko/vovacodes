27ab08a6ad881c8e152117784a1f45f5fb38d514
vovaushenko <vovaushenko1989@gmail.com>
2021-08-27 19:41:21 +0300

    feat(components): add new Recommended component with tests and story

---
+++ components/Recommended/Recommended.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import * as Styled from './Recommended.styles';
+import { useRecommendedConfig } from './Recommended.config';
+import DesktopButton from '../DesktopButton/DesktopButton';
+
+/**
+ *Renders section with recommended apps and recently opened files
+ *@function Recommended
+ *@returns {JSX.Element} - Rendered Recommended component
+ */
+const Recommended = (): JSX.Element => {
+  const { recommendedFiles } = useRecommendedConfig();
+  return (
+    <Styled.Container>
+      {recommendedFiles.map((file) => (
+        <DesktopButton
+          key={file.id}
+          variant={'recommendedApp'}
+          iconSrc={file.icon}
+          iconSize={file.iconSize}
+          text={file.fileName}
+          details={file.details}
+          onClick={file.action}
+        />
+      ))}
+    </Styled.Container>
+  );
+};
+export default Recommended;
